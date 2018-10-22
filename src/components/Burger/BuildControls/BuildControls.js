38a63bfbacc56cb09fee338c8d936dced776f6e2
Yiran Zhou <yzhou@indeed.com>
2018-10-22 14:58:05 -0500

    Custom Button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,12 +11,20 @@ const controls =[
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(control => (
             <BuildControl
                 key={control.label}
                 label={control.label}
-                added={() => props.ingredientAdded(control.type)}/>
+                added={() => props.ingredientAdded(control.type)}
+                removed={() => props.ingredientRemoved(control.type)}
+                disabled={props.disabled[control.type]}
+            />
         ))}
+        <button
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>Order Now</button>
     </div>
 );
 
