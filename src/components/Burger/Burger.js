ec6e15a413a37c7a7a618a602c7753d01195ee17
Yiran Zhou <yzhou@indeed.com>
2018-10-22 11:52:22 -0500

    can add items

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,25 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 
 const burger = (props) => {
+    // ingredients object, need to transform it into an array
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        }).reduce((arr, el) => {
+            // loop through all the elements and add them to the initial values
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Pleast start adding ingredients!</p>
+    }
+
     return (
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top"/>
-          <BurgerIngredient type="cheese"/>
-          <BurgerIngredient type="meat"/>
-          {/*<BurgerIngredient type="bacon"/>*/}
-          {/*<BurgerIngredient type="salad"/>*/}
+          {transformedIngredients}
           <BurgerIngredient type="bread-bottom"/>
       </div>
     );
